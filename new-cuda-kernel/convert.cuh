664f2cc8057cae55ad970b131c3df9b48b981a21
isotr0py <2037008807@qq.com>
2024-12-13 19:00:09 +0800

    fix q3k dequant
    Signed-off-by: isotr0py <2037008807@qq.com>

--- new-cuda-kernel/convert.cuh
+++ new-cuda-kernel/convert.cuh
@@ -135,14 +135,14 @@ static __global__ void dequantize_block_q3_K(const void * __restrict__ vx, dst_t
                 is <  8 ? (x[i].scales[is-0] & 0xF) | (((x[i].scales[is+4] >> 2) & 3) << 4) :
                 is < 12 ? (x[i].scales[is-8] >>  4) | (((x[i].scales[is+0] >> 4) & 3) << 4) :
                           (x[i].scales[is-8] >>  4) | (((x[i].scales[is-4] >> 6) & 3) << 4);
-    float d_all = __half2float(x[i].d);
-    float dl = d_all * (us - 32);
+    half d_all = x[i].d;
+    half dl = __hmul(d_all,  __int2half_rn(us - 32));
 
     dst_t * y = yy + i*QK_K + 128*n + 32*j;
     const uint8_t * q = x[i].qs + 32*n;
     const uint8_t * hm = x[i].hmask;
 
-    for (int l = l0; l < l0+4; ++l) y[l] = dl * ((int8_t)((q[l] >> shift) & 3) - ((hm[l] & m) ? 0 : 4));
+    for (int l = l0; l < l0+4; ++l) y[l] = __hmul(dl,  __int2half_rn((int8_t)((q[l] >> shift) & 3) - ((hm[l] & m) ? 0 : 4)));
 }
 
 static inline __device__ void get_scale_min_k4(int j, const uint8_t * q, uint8_t & d, uint8_t & m) {
