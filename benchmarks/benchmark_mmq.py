239c8fded1ab7fd8bac50a9e3a96975498f624de
Isotr0py <mozf@mail2.sysu.edu.cn>
2025-08-09 22:18:18 +0800

    [1/N] Update MMQ kernel catch up upstream llama.cpp implementation (#5)
    * draft
    
    Signed-off-by: Isotr0py <2037008807@qq.com>
    
    * add benchmark script
    
    Signed-off-by: isotr0py <2037008807@qq.com>
    
    * update
    
    Signed-off-by: Isotr0py <2037008807@qq.com>
    
    * fix shmem
    
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>
    
    * update benchmark
    
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>
    
    * use csv
    
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>
    
    ---------
    
    Signed-off-by: Isotr0py <2037008807@qq.com>
    Signed-off-by: isotr0py <2037008807@qq.com>
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

---
+++ benchmarks/benchmark_mmq.py
@@ -0,0 +1,148 @@
+import argparse
+import json
+import time
+from functools import cache
+
+import pandas as pd
+import torch
+from gguf import GGMLQuantizationType
+from utils import get_gguf_sample_tensors, seed_everything
+
+QUANT_TYPES_MAP = {
+    "Q2_K": GGMLQuantizationType.Q2_K,
+    "Q3_K": GGMLQuantizationType.Q3_K,
+    "Q4_K": GGMLQuantizationType.Q4_K,
+    "Q5_K": GGMLQuantizationType.Q5_K,
+    "Q6_K": GGMLQuantizationType.Q6_K,
+    "Q4_0": GGMLQuantizationType.Q4_0,
+    "Q5_0": GGMLQuantizationType.Q5_0,
+    "Q8_0": GGMLQuantizationType.Q8_0,
+}
+
+DTYPES_MAP = {
+    "half": torch.float16,
+    "bfloat16": torch.bfloat16,
+    "float": torch.float32,
+}
+
+
+@cache
+def get_kernel_ops(use_remote: bool):
+    if use_remote:
+        from kernels import get_kernel
+
+        ops = get_kernel("Isotr0py/ggml")
+    else:
+        import ggml as ops
+    return ops
+
+
+@torch.inference_mode()
+def main(
+    num_tokens: int,
+    hidden_size: int,
+    quant_type: GGMLQuantizationType,
+    dtype: torch.dtype,
+    seed: int = 0,
+    do_profile: bool = False,
+    num_warmup_iters: int = 5,
+    num_iters: int = 100,
+    use_remote: bool = False,
+) -> None:
+    ops = get_kernel_ops(use_remote)
+
+    seed_everything(seed)
+    torch.set_default_device("cuda")
+
+    x = torch.randn(num_tokens, hidden_size, dtype=dtype)
+    w = [
+        torch.tensor(tensor.data, device="cuda")
+        for tensor in get_gguf_sample_tensors(
+            hidden_size=hidden_size, quant_type=quant_type
+        )
+    ]
+
+    def run_cuda_benchmark(num_iters: int, profile: bool = False) -> float:
+        torch.cuda.synchronize()
+        if profile:
+            torch.cuda.cudart().cudaProfilerStart()
+        start_time = time.perf_counter()
+
+        for _ in range(num_iters):
+            for tensor in w:
+                ops.ggml_mul_mat_a8(
+                    tensor,
+                    x,
+                    quant_type,
+                    tensor.size(0),
+                )
+
+        torch.cuda.synchronize()
+
+        end_time = time.perf_counter()
+        if profile:
+            torch.cuda.cudart().cudaProfilerStop()
+        return (end_time - start_time) / num_iters
+
+    # Warmup.
+    run_benchmark = run_cuda_benchmark
+    run_benchmark(num_iters=num_warmup_iters, profile=False)
+
+    # Benchmark.
+    if do_profile:
+        latency = run_benchmark(num_iters=1, profile=True)
+    else:
+        latency = run_benchmark(num_iters=num_iters, profile=False)
+
+    quant_name = [
+        name for name, qtype in QUANT_TYPES_MAP.items() if qtype == quant_type
+    ][0]
+    print(f"{quant_name} Kernel running time: {latency * 1e3 :.3f} ms")
+    return latency * 1e3
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Benchmark the MMQ kernel for matrix multiplication with quantized weights"
+    )
+    parser.add_argument("--num-tokens", type=int, default=4096)
+    parser.add_argument("--hidden-size", type=int, choices=[256, 1024], default=1024)
+    parser.add_argument(
+        "--quant-dtype", type=str, choices=QUANT_TYPES_MAP.keys(), default="Q4_0"
+    )
+    parser.add_argument("--dtype", type=str, choices=DTYPES_MAP.keys(), default="half")
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--profile", action="store_true")
+    parser.add_argument("--use-remote", action="store_true", help="Use remote kernels")
+    parser.add_argument("--num-warmup-iters", type=int, default=5)
+    parser.add_argument(
+        "--num-iters",
+        type=int,
+        default=100,
+        help="Number of benchmark iterations. "
+        "If --profile is set, this number is ignored",
+    )
+
+    args = parser.parse_args()
+    print(args)
+
+    dtype = DTYPES_MAP[args.dtype]
+    result = {"Quantization": [], "Time (ms)": []}
+    for quant_name, quant_type in QUANT_TYPES_MAP.items():
+        latency = main(
+            num_tokens=args.num_tokens,
+            hidden_size=args.hidden_size,
+            quant_type=quant_type,
+            dtype=dtype,
+            seed=args.seed,
+            do_profile=args.profile,
+            num_warmup_iters=args.num_warmup_iters,
+            num_iters=args.num_iters,
+            use_remote=args.use_remote,
+        )
+        result["Quantization"].append(quant_name)
+        result["Time (ms)"].append(latency)
+
+    kernel_mode = "remote" if args.use_remote else "local"
+    result_df = pd.DataFrame(result)
+    result_df.to_csv(f"benchmark_results_{kernel_mode}_HD{args.hidden_size}xB{args.num_tokens}.csv", index=False)
---
+++ hf-kernels/ggml-kernels/ggml/mmq.cu
@@ -0,0 +1,158 @@
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
+
+#include <torch/all.h>
+#include <c10/cuda/CUDAGuard.h>
+
+#include "cuda_compat.h"
+#include "cuda_utils.h"
+#include "dispatch_utils.h"
+
+#include "ggml-common.h"
+#include "vecdotq.cuh"
+#include "mmq.cuh"
+
+
+int64_t ggml_get_block_size(int64_t type) {
+    switch (type) {
+        case GGML_TYPE_Q4_0:    return QK4_0;
+        case GGML_TYPE_Q4_1:    return QK4_1;
+        case GGML_TYPE_Q5_0:    return QK5_0;
+        case GGML_TYPE_Q5_1:    return QK5_1;
+        case GGML_TYPE_Q8_0:    return QK8_0;
+        case GGML_TYPE_Q8_1:    return QK8_1;
+        case GGML_TYPE_Q2_K:    return QK_K;
+        case GGML_TYPE_Q3_K:    return QK_K;
+        case GGML_TYPE_Q4_K:    return QK_K;
+        case GGML_TYPE_Q5_K:    return QK_K;
+        case GGML_TYPE_Q6_K:    return QK_K;
+        case GGML_TYPE_IQ2_XXS: return QK_K;
+        case GGML_TYPE_IQ2_XS:  return QK_K;
+        case GGML_TYPE_IQ2_S:   return QK_K;
+        case GGML_TYPE_IQ3_XXS: return QK_K;
+        case GGML_TYPE_IQ3_S:   return QK_K;
+        case GGML_TYPE_IQ1_S:   return QK_K;
+        case GGML_TYPE_IQ1_M:   return QK_K;
+        case GGML_TYPE_IQ4_NL:  return QK4_NL;
+        case GGML_TYPE_IQ4_XS:  return QK_K;
+        default: return 0; // unsupported type
+    }
+}
+
+
+// Q8 gemv
+template <typename scalar_t>
+static __global__ void quantize_q8_1(const scalar_t* __restrict__ x,
+                                     void* __restrict__ vy, const int kx,
+                                     const int kx_padded) {
+  const int ix = blockDim.x * blockIdx.x + threadIdx.x;
+  if (ix >= kx_padded) {
+    return;
+  }
+  const int iy = blockDim.y * blockIdx.y + threadIdx.y;
+  const int i_padded = iy * kx_padded + ix;
+
+  block_q8_1* y = (block_q8_1*)vy;
+
+  const int ib = i_padded / QK8_1;   // block index
+  const int iqs = i_padded % QK8_1;  // quant index
+
+  const float xi = ix < kx ? static_cast<float>(x[iy * kx + ix]) : 0.0f;
+  float amax = fabsf(xi);
+  float sum = xi;
+
+#pragma unroll
+  for (int mask = 16; mask > 0; mask >>= 1) {
+    amax = fmaxf(amax, VLLM_SHFL_XOR_SYNC_WIDTH(amax, mask, 32));
+    sum += VLLM_SHFL_XOR_SYNC_WIDTH(sum, mask, 32);
+  }
+
+  const float d = amax / 127;
+  const int8_t q = amax == 0.0f ? 0 : roundf(xi / d);
+
+  y[ib].qs[iqs] = q;
+
+  if (iqs > 0) {
+    return;
+  }
+
+  y[ib].ds.x = __float2half(d);
+  y[ib].ds.y = __float2half(sum);
+}
+
+template <typename scalar_t>
+static void quantize_row_q8_1_cuda(const scalar_t* x, void* vy, const int kx,
+                                   const int ky, cudaStream_t stream) {
+  const int64_t kx_padded = (kx + 512 - 1) / 512 * 512;
+  const int block_num_x =
+      (kx_padded + CUDA_QUANTIZE_BLOCK_SIZE - 1) / CUDA_QUANTIZE_BLOCK_SIZE;
+  constexpr int MAX_BLOCK_SIZE = 65535;
+  for (int off = 0; off < ky; off += MAX_BLOCK_SIZE) {
+    const int num_blocks_y = std::min(ky, off + MAX_BLOCK_SIZE) - off;
+    const dim3 num_blocks(block_num_x, num_blocks_y, 1);
+    const dim3 block_size(CUDA_DEQUANTIZE_BLOCK_SIZE, 1, 1);
+    quantize_q8_1<<<num_blocks, block_size, 0, stream>>>(
+        &x[off * kx], (int32_t*)vy + off * (kx_padded / 32 * 9), kx, kx_padded);
+  }
+}
+
+
+torch::Tensor ggml_mul_mat_a8(torch::Tensor W,  // quant weight
+                              torch::Tensor X,  // input
+                              int64_t type, int64_t row) {
+  int col = X.sizes()[1];
+  int padded = (col + 512 - 1) / 512 * 512;
+  int batch = X.sizes()[0];
+  const at::cuda::OptionalCUDAGuard device_guard(device_of(X));
+  auto options = torch::TensorOptions().dtype(X.dtype()).device(W.device());
+  at::Tensor Y = torch::empty({batch, row}, options);
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream().stream();
+  options = torch::TensorOptions().dtype(torch::kInt32).device(W.device());
+  at::Tensor quant_X = torch::empty({batch, padded / 32 * 9}, options);
+  VLLM_DISPATCH_FLOATING_TYPES(X.scalar_type(), "ggml_mul_mat_a8", [&] {
+    quantize_row_q8_1_cuda((scalar_t*)X.data_ptr(), (void*)quant_X.data_ptr(),
+                           col, batch, stream);
+
+    const int64_t stride00 = col / ggml_get_block_size(type);
+    mmq_args<scalar_t> kernel_args;
+    kernel_args = {
+        (char*)W.data_ptr(), (char*)quant_X.data_ptr(),
+        (scalar_t*)Y.data_ptr(), col, row, stride00, padded, batch, row
+    };
+
+    switch (type) {
+      case GGML_TYPE_Q4_0:
+        mul_mat_q_case<scalar_t, GGML_TYPE_Q4_0>(kernel_args, stream);
+        break;
+      case GGML_TYPE_Q4_1:
+        mul_mat_q_case<scalar_t, GGML_TYPE_Q4_1>(kernel_args, stream);
+        break;
+      case GGML_TYPE_Q5_0:
+        mul_mat_q_case<scalar_t, GGML_TYPE_Q5_0>(kernel_args, stream);
+        break;
+      case GGML_TYPE_Q5_1:
+        mul_mat_q_case<scalar_t, GGML_TYPE_Q5_1>(kernel_args, stream);
+        break;
+      case GGML_TYPE_Q8_0:
+        mul_mat_q_case<scalar_t, GGML_TYPE_Q8_0>(kernel_args, stream);
+        break;
+      case GGML_TYPE_Q2_K:
+        mul_mat_q_case<scalar_t, GGML_TYPE_Q2_K>(kernel_args, stream);
+        break;
+      case GGML_TYPE_Q3_K:
+        mul_mat_q_case<scalar_t, GGML_TYPE_Q3_K>(kernel_args, stream);
+        break;
+      case GGML_TYPE_Q4_K:
+        mul_mat_q_case<scalar_t, GGML_TYPE_Q4_K>(kernel_args, stream);
+        break;
+      case GGML_TYPE_Q5_K:
+        mul_mat_q_case<scalar_t, GGML_TYPE_Q5_K>(kernel_args, stream);
+        break;
+      case GGML_TYPE_Q6_K:
+        mul_mat_q_case<scalar_t, GGML_TYPE_Q6_K>(kernel_args, stream);
+        break;
+    }
+  });
+  return Y;
+}
