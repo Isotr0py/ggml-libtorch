fix test overflow
Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

hf-kernels/ggml-kernels/tests/kernels/test_cuda_kernels.py
@@ -159,7 +159,7 @@ def test_mmq_batching(
     seed_everything(0)
 
     tensors = get_gguf_sample_tensors(hidden_size, quant_type)
-    x = torch.rand((batch_size, num_tokens, hidden_size), dtype=dtype, device="cuda")
+    x = torch.rand((batch_size, num_tokens, hidden_size), dtype=dtype, device="cuda") * 1e-1
     for tensor in tensors:
         weight = torch.tensor(dequantize(tensor.data, quant_type), device="cuda").to(
             dtype
@@ -169,11 +169,11 @@ def test_mmq_batching(
         qweight = torch.tensor(tensor.data, device="cuda")
         output = ops.ggml_mul_mat_a8(qweight, x, quant_type, qweight.shape[0]).to(dtype)
 
-        atols = {torch.half: 1, torch.bfloat16: 1.5, torch.float: 1.2}
+        atols = {torch.half: 1.2e-1, torch.bfloat16: 1.5, torch.float: 1.2e-1}
         # test matrix has inputs centered around 0 and lower precision from
         # bfloat16 tends to accumulate and can greatly inflate rtol
         # since outputs are also very close to 0
-        rtols = {torch.half: 1e-1, torch.bfloat16: 1e4, torch.float: 2e1}
+        rtols = {torch.half: 1e-1, torch.bfloat16: 1e-1, torch.float: 2e-1}
         torch.testing.assert_close(
             output, ref_output, atol=atols[dtype], rtol=rtols[dtype]
         )