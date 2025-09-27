20dd0096375ecc3101fa064bb908723daf7f6cd2
Isotr0py <mozf@mail2.sysu.edu.cn>
2025-09-27 23:03:58 +0800

    update
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

--- hf-kernels/ggml-kernels/ggml/mmq.cuh
+++ hf-kernels/ggml-kernels/ggml/mmq.cuh
@@ -1308,3 +1308,12 @@ extern DECL_MMQ_CASE(half, GGML_TYPE_Q6_K);
 
 // bf16 kernel
 extern DECL_MMQ_CASE(__nv_bfloat16, GGML_TYPE_Q4_0);
+extern DECL_MMQ_CASE(__nv_bfloat16, GGML_TYPE_Q4_1);
+extern DECL_MMQ_CASE(__nv_bfloat16, GGML_TYPE_Q5_0);
+extern DECL_MMQ_CASE(__nv_bfloat16, GGML_TYPE_Q5_1);
+extern DECL_MMQ_CASE(__nv_bfloat16, GGML_TYPE_Q8_0);
+extern DECL_MMQ_CASE(__nv_bfloat16, GGML_TYPE_Q2_K);
+extern DECL_MMQ_CASE(__nv_bfloat16, GGML_TYPE_Q3_K);
+extern DECL_MMQ_CASE(__nv_bfloat16, GGML_TYPE_Q4_K);
+extern DECL_MMQ_CASE(__nv_bfloat16, GGML_TYPE_Q5_K);
+extern DECL_MMQ_CASE(__nv_bfloat16, GGML_TYPE_Q6_K);
