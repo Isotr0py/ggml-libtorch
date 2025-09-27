d191ef234600dc937588f69ae7381a5ce9115a9e
Isotr0py <mozf@mail2.sysu.edu.cn>
2025-09-28 02:01:17 +0800

    update
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

--- hf-kernels/ggml-kernels/ggml/mmq.cu
+++ hf-kernels/ggml-kernels/ggml/mmq.cu
@@ -1,17 +1,17 @@
-// #include <cuda.h>
-// #include <cuda_fp16.h>
-// #include <cuda_runtime.h>
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
 
-// #include <torch/all.h>
-// #include <c10/cuda/CUDAGuard.h>
+#include <torch/all.h>
+#include <c10/cuda/CUDAGuard.h>
 
-// #include "cuda_compat.h"
-// #include "cuda_utils.h"
-// #include "dispatch_utils.h"
+#include "cuda_compat.h"
+#include "cuda_utils.h"
+#include "dispatch_utils.h"
 
-// #include "ggml-common.h"
-// #include "vecdotq.cuh"
-#include "kernel_instances/mmq_kernel_template.h"
+#include "ggml-common.h"
+#include "vecdotq.cuh"
+#include "mmq.cuh"
 
 
 cuda_device_info get_cuda_info() {
