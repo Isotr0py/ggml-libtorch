41c4591546415447f704d10fb9b3472d52e783e2
Isotr0py <mozf@mail2.sysu.edu.cn>
2025-10-06 10:57:57 +0800

    clean unused header
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

--- hf-kernels/ggml-kernels/ggml/kernel_instances/mmq_kernel.cuh
+++ hf-kernels/ggml-kernels/ggml/kernel_instances/mmq_kernel.cuh
@@ -1,11 +1,5 @@
 #pragma once
 
-#include <cuda.h>
-#include <cuda_fp16.h>
-#include <cuda_runtime.h>
-
-#include <c10/cuda/CUDAGuard.h>
-
 #include "../cuda_compat.h"
 #include "../cuda_utils.h" 
 #include "../dispatch_utils.h"
--- hf-kernels/ggml-kernels/ggml/mmq.cu
+++ hf-kernels/ggml-kernels/ggml/mmq.cu
@@ -1,7 +1,3 @@
-#include <cuda.h>
-#include <cuda_fp16.h>
-#include <cuda_runtime.h>
-
 #include <torch/all.h>
 #include <c10/cuda/CUDAGuard.h>
 
